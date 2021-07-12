ae4b3c1317324d0edc336f6126b3321542fddeda
Marek Štefanko <marcostesvk@gmail.com>
2021-07-12 18:00:44 +0200

    added middlewares

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
+import { useDispatch, useSelector } from "react-redux";
+import { decrement, increment } from "./store/action";
+
 function App() {
+  const count = useSelector((state) => state);
+  const dispatch = useDispatch();
   return (
     <div className="App">
-      <h1>Toto je basic setup TAILWINDCSS/REDUX</h1>
+      <h1>{count}</h1>
+      <button onClick={() => dispatch(decrement())}>DECREMENT</button>
+      <button onClick={() => dispatch(increment())}>INCREMENT</button>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,11 +2,45 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import { applyMiddleware, createStore } from "redux";
+import { counterReducer } from "./store/reducer";
+import { Provider } from "react-redux";
+import logger from "redux-logger";
+/* const myLogger = (store) => {
+  return (next) => {
+    return (action) => {
+      console.log("middleware ran");
+      return next(action);
+    };
+  };
+}; */
+
+const myLogger = (store) => (next) => (action) => {
+  console.log("middleware ran");
+  return next(action);
+};
+
+const secondMiddleware = (store) => (next) => (action) => {
+  console.log("secondMiddleware ran");
+  return next(action);
+};
+
+const capAtTen = (store) => (next) => (action) => {
+  if (store.getState() >= 10) {
+    return next({ type: "DECREMENT" });
+  }
+  next(action);
+};
+
+const store = createStore(
+  counterReducer,
+  applyMiddleware(myLogger, secondMiddleware, capAtTen, logger)
+);
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById("root")
 );
 
